e4d3573c8911d3ed9aac0814785da9e779de6a53
= <=>
2012-05-12 12:31:39 +0100

    Added new project, gems, unicorn for dev and foreman config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionServices::Application.config.secret_token = '8355d11cf3157038ee35ef23bcc5ac7351a59eb5945d6364649af03f92184f078b0b497a23b1ce5acb6e96bd0210f6893abe009b581ee8e108720c6ba65f8919'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+QuestionServices::Application.config.session_store :cookie_store, key: '_question-services_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# QuestionServices::Application.config.session_store :active_record_store
