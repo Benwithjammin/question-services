71388232c75b0e477e8f84237ea8833fdee862b3
Ben Porter <Jammin1984@gmail.com>
2012-05-21 23:03:43 +0100

    More coverage of question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -18,14 +18,58 @@ describe Question do
 
   end
 
-  describe "validation" do
+  describe "validation specs" do
 
     let(:question) { Fabricate.build(:question)}
 
-    it "title should add error when blank" do
+    it "empty title should be invalid" do
       question.title = ""
-      question.save
-      question.errors.empty? should be false
+      question.should_not be_valid
+      question.should have(1).errors_on(:title)
+    end
+
+    context "question_type" do
+
+      it "'text' is valid" do
+        question.question_type = "text"
+        question.should be_valid
+      end
+
+      it "'image' is valid" do
+        question.question_type = "image"
+        question.should be_valid
+      end
+
+      it "anything not 'image' or 'text' is invalid" do
+        question.question_type = Forgery(:basic).text
+        question.should_not be_valid
+        question.should have(1).errors_on(:question_type)
+      end
+
+    end
+
+    context "user" do
+
+      let(:user) { Fabricate(:user) }
+      let(:question) { Fabricate(:question) }
+
+      it "persisted user is valid" do
+        question.user = user
+        question.should be_valid
+      end
+
+      it "not persisted user is invalid" do
+        question.user = User.new
+        question.should_not be_valid
+        question.should have(1).errors_on(:user)
+      end
+
+      it "null user should invalid" do
+        question.user = nil
+        question.should_not be_valid
+        question.should have(1).errors_on(:user)
+      end
+
     end
 
   end
