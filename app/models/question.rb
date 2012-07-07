a3ef59ecf976ad29d4ebfb21bcf35f16fb05e102
= <=>
2012-07-07 13:40:49 +0100

    Sorted out nested querries for questions answered and unanswered by user

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,11 +4,8 @@ class Question < ActiveRecord::Base
   attr_reader :user_answer
   belongs_to :user
   has_many :answers
-  has_many :responses
 
   QUESTION_TYPES = [ "text", "image" ]
-
-  scope :unanswered_by_user, -> id { joins { answers.responses }.where{ answers.responses.user_id << id }.uniq }
 
   validates :title, :user, presence: true
   validates :answers, length: { maximum: 26 }
@@ -25,14 +22,21 @@ class Question < ActiveRecord::Base
   end
 
   def self.answered_by_user(user_id)
-    Question.joins { answers.responses }
-            .where{ answers.responses.user_id >> user_id }.uniq
-            .each{ |q| q.user_answer = user_id }
+    Question.where { id >> Question.user_response_question_ids(user_id) }
+  end
+
+  def self.unanswered_by_user(user_id)
+    Question.where { id << Question.user_response_question_ids(user_id) }
   end
 
   def user_answer=(user_id)
     selected_response = answers.joins { responses }.where{ responses.user_id == user_id }.first
     @user_answer = selected_response.nil? ? 0 : selected_response.id
+  end
+
+  private
+  def self.user_response_question_ids(user_id)
+    Response.where { user_id == user_id }.collect { |r| r.question_id }
   end
 
   #self.user = User.find_or_initialize_by_id(user_id)
